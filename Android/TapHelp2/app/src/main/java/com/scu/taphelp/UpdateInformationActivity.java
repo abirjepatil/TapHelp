706f27f2935df7fb7f92c34320485777eb89c361
rachnagajre <rachi.g12@gmail.com>
2016-04-10 23:40:56 -0700

    Added Action Bar with back buttons on Update Account information UI (Clicking on Back button from Update Account Settings will return user back to main settings UI)

--- Android/TapHelp2/app/src/main/java/com/scu/taphelp/UpdateInformationActivity.java
+++ Android/TapHelp2/app/src/main/java/com/scu/taphelp/UpdateInformationActivity.java
@@ -1,13 +1,44 @@
 package com.scu.taphelp;
 
 import android.os.Bundle;
+import android.support.v4.app.NavUtils;
 import android.support.v7.app.AppCompatActivity;
+import android.view.MenuItem;
+import android.view.View;
 
 public class UpdateInformationActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_update_information);
+        setContentView(R.layout.activity_update_account_info);
+
+        android.support.v7.widget.Toolbar toolbar = (android.support.v7.widget.Toolbar) findViewById(R.id.toolbar);
+        toolbar.setNavigationIcon(R.drawable.abc_ic_ab_back_mtrl_am_alpha);
+        toolbar.setTitle("Update Account Settings");
+        toolbar.setNavigationOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                NavUtils.navigateUpFromSameTask(UpdateInformationActivity.this);
+            }
+        });
+        setSupportActionBar(toolbar);
+        android.support.v7.app.ActionBar actionBar = getSupportActionBar();
+        actionBar.setDisplayHomeAsUpEnabled(true);
+        actionBar.setDefaultDisplayHomeAsUpEnabled(true);
+        actionBar.setDisplayShowHomeEnabled(true);
+        actionBar.setHomeButtonEnabled(true);
+
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case android.R.id.home:
+                finish();
+                return true;
+            default:
+                return super.onOptionsItemSelected(item);
+        }
     }
 }
